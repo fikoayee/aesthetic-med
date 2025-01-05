1ed9b8d7f80d9089fdf9318cd905ad120c92fe79
unknown <dziubekfilip@gmail.com>
2025-01-05 17:20:01 +0100

    feat: add logo & add navigation icons

---
+++ src/app/ui/Logo/styles.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import styled from 'styled-components';
+
+export const StyledBoxWrapper = styled(Box)({
+    display: 'flex',
+    alignItems: 'center'
+})
+export const StyledTypography = styled(Typography)({
+    fontSize: '1.6rem',
+    fontWeight: '700',
+    background: 'linear-gradient(90deg, #79acf7, #2396e8)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    display: 'inline-block',
+
+})
